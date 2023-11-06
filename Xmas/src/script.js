19dcdf873aad8a510666a6c301f0837e23d8d8c8
daomintam <minhtam030797@gmail.com>
2023-11-06 16:00:06 +0700

    i

--- Xmas/src/script.js
+++ Xmas/src/script.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
       $card = $('.card');
       var $musicFrame = $('#musicFrame');
       $musicFrame.hide();
-      
+  const greeting = document.getElementById('greeting');
   $card.on('click', function() {
- 
+    greeting.style.display = 'block';
 		$(this).toggleClass('is-opened');
     $clickMe.toggleClass('is-hidden');
     if ($musicFrame.is(":hidden")) {
