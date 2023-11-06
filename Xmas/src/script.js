508ba0ac0af7b3111e1eb811020c1d3f0d66ee8b
daomintam <minhtam030797@gmail.com>
2023-11-06 15:09:23 +0700

    i

--- Xmas/src/script.js
+++ Xmas/src/script.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
   var $clickMe = $('.click-icon'),
       $card = $('.card');
-
+      var $musicFrame = $('#musicFrame');
+      $musicFrame.hide();
+      
   $card.on('click', function() {
  
 		$(this).toggleClass('is-opened');
     $clickMe.toggleClass('is-hidden');
-    // if ($(this).hasClass('is-opened')) {
-    //   typeText();
-    // }
- 
+    if ($musicFrame.is(":hidden")) {
+      $musicFrame.show();
+    } else {
+      $musicFrame.hide();
+    }
 	});
 });
